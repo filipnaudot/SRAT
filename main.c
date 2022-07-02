8af8ddfc7dc852d22495cd5ab361cc7edf51db24
filip <filip.naudot@gmail.com>
2022-07-02 22:18:39 +0000

    Added main file

---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+ * @file main.c
+ * @author Filip Naudot
+ * @brief Remote acces tool capable of remote command execution aswell as file upload and download
+ * @version 0.1
+ * @date 2022-07-02
+ * 
+ */
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
