8e0e0eade828075c8fedac98a3934daf0da30527
filip <filip.naudot@gmail.com>
2022-07-26 17:27:01 +0200

    work on file transfer

--- local.c
+++ local.c
@@ -113,17 +113,71 @@ int main(int argc, char *argv[]) {
             if (strncmp("get ", data.read, 4) == 0) {
                 printf("START FILE TRANSFER\n");
                 data.is_get = true;
+                int i;
+                for (i =  0; i < strlen(data.read) - 4; i++) {
+                    data.read[i] = data.read[i+4];
+                }
+                data.read[i] = '\0';
             }
 
             send(socket_peer, &data.is_get, sizeof(data.is_get), 0); 
             int bytes_sent = send(socket_peer, data.read, strlen(data.read), 0); 
+            
             #ifdef VERBOSE
             printf("Sent %d bytes.\n", bytes_sent);
             #endif
+            
+            if (data.is_get) {
+                write_file(socket_peer);
+            }
         }
     } //end while(1)
     
     close(socket_peer);
 
     return 0;
+}
+
+
+void write_file(int socket_peer) {
+    int n;
+    FILE *fp;
+    char *filename = "recv.txt";
+    char buffer[1024];
+    
+    printf("RECIEVING FILE...\n");
+    fp = fopen(filename, "w");
+    
+    int i = 0;
+    while (1) {
+        printf("%d\n", i++);
+
+        fd_set reads;
+        FD_ZERO(&reads);
+        FD_SET(socket_peer, &reads);
+        FD_SET(STDIN_FILENO, &reads);
+
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 100000;
+    
+        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
+            perror("select");
+            exit(1);
+        }
+
+        if (FD_ISSET(socket_peer, &reads)) {
+            int n = recv(socket_peer, buffer, 1024, 0);
+            if (n < 1) {
+                break;
+            }
+            printf("Recieved %d bytes\n", n);
+            printf("RECEIVED: [%s]\n", buffer);
+        
+            fprintf(fp, "%s", buffer);
+            bzero(buffer, 1024);
+        }
+    }
+    fclose(fp);
+    printf("FILE RECIEVED\n");
 }
