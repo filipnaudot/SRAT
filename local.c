82fdd158e49191088480d09638db95a561229a27
filip <filip.naudot@gmail.com>
2022-07-31 11:39:40 +0200

    using transfer.c in local

--- local.c
+++ local.c
@@ -9,6 +9,7 @@
  */
 
 #include "srat.h"
+#include "transfer.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -153,63 +154,6 @@ int main(int argc, char *argv[]) {
     close(socket_peer);
 
     return 0;
-}
-
-
-void send_file(FILE *fp, int sockfd, long file_size) {
-    int n;
-    char data[1024] = {0};
-
-    if (send(sockfd, &file_size, sizeof(long), 0) < 0) {
-        perror("send");
-        exit(1);
-    }
-
-    while (fgets(data, 1024, fp) != NULL) {
-        if (send(sockfd, data, strlen(data), 0) < 0) {
-            perror("send");
-            exit(1);
-        }
-        bzero(data, 1024);
-    }
-}
-
-
-void write_file(int socket_peer, char* filename) {
-    int n;
-    FILE *fp;
-    char buffer[1024];
-    long file_size = 0;
-    long total_bytes_recieved = 0;
-
-    fp = fopen(filename, "w");
-    // recieve file size
-    recv(socket_peer, &file_size, sizeof(long), 0);
-
-    do {
-        fd_set reads;
-        FD_ZERO(&reads);
-        FD_SET(socket_peer, &reads);
-
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 100000;
-    
-        if (select(socket_peer+1, &reads, 0, 0, &timeout) < 0) {
-            perror("select");
-            exit(1);
-        }
-
-        if (FD_ISSET(socket_peer, &reads)) {
-            int n = recv(socket_peer, buffer, 1024, 0);
-            total_bytes_recieved += n;
-        
-            fprintf(fp, "%s", buffer);
-            bzero(buffer, 1024);
-        }
-    } while(total_bytes_recieved < file_size);
-
-    fclose(fp);
 }
 
 
